f4dab6aa6378de3524884b9c3fe916c0502b6f90
Aparna <aparna@metricsatsocial.in>
2022-10-19 13:00:44 +0530

    material theme added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
 
-  constructor() { }
+export class LoginComponent implements OnInit {
+  username: string = "";
+  password: string = "";
+  show: boolean = false;
+  loginForm: FormGroup | any;
+
+  constructor(private router:Router) {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email, Validators.pattern(
+        '[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,63}$',
+      ),]),
+      password: new FormControl('', [Validators.required, Validators.pattern(
+        '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,12}$'
+      )])
+    });
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    if (!this.loginForm.valid) {
+      return;
+    }
+    localStorage.setItem('user', this.loginForm.value)
+    this.router.navigate(['/home'])
+  }
+  clear() {
+    this.username = "";
+    this.password = "";
+    this.show = true;
   }
 
 }

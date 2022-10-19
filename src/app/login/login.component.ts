3ee70881e6817d77de212348424e2d26ee49edcb
Aparna <aparna@metricsatsocial.in>
2022-10-19 15:18:27 +0530

    login component added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -19,9 +19,7 @@ export class LoginComponent implements OnInit {
       email: new FormControl('', [Validators.required, Validators.email, Validators.pattern(
         '[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,63}$',
       ),]),
-      password: new FormControl('', [Validators.required, Validators.pattern(
-        '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,12}$'
-      )])
+      password: new FormControl('', [Validators.required])
     });
   }
 
